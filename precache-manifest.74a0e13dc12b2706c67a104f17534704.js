87efcf7efa857ffc75beaa78b3baa22a91aa5c4d
sarthak kanungo <skanungodgacm@gmail.com>
2020-04-04 20:42:33 +0530

    Updates

---
+++ precache-manifest.74a0e13dc12b2706c67a104f17534704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae908cd94ba92aa4cd9bec0ce773516",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ce7ee805439ff7274356",
+    "url": "/monsters-rolodex/static/css/main.2499cf8e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7ee805439ff7274356",
+    "url": "/monsters-rolodex/static/js/main.07464ca2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
